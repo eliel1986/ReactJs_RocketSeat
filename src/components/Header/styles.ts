3150c6ae160493ee2394f54964c530bcf9335402
Eliel Silva <79846053+eliel1986@users.noreply.github.com>
2022-05-12 01:42:28 -0300

    stylesContent

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,3 +3,29 @@ import styled from 'styled-components';
 export const Container = styled.header`
     background: var(--blue);
 `;
+
+export const Content = styled.div`
+max-width: 1120px;
+margin: 0 auto;
+
+padding: 0 1rem 10rem; /*rem: valor multiplicado pelo font-size do HTML*/
+display: flex;
+align-items: center;    
+justify-content:  space-between;  /*espaço entre itens*/
+
+button {
+    font-size: 1rem;
+    color: #FFF;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s;
+
+    &:hover {
+        filter: brightness(0.9);
+    }
+}
+`;
